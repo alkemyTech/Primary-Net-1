1593278ac85ae8381a3248d396ccb4d28f92cf79
juanpaladino15 <juanpaladino15@gmail.com>
2023-03-23 17:27:14 -0300

    add: Entity Account y su respectiva referencia en DbContext

---
+++ Wallet-grupo1/Entidades/Account.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wallet_grupo1.Entidades
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public DateOnly creationDate { get; set; }
+        public double money { get; set; }
+        public bool isBlocked { get; set; }
+        public int user_id { get; set; }
+
+        [ForeignKey("user_id")]
+        public virtual int User_id{ get; set; }
+
+    }
+}
