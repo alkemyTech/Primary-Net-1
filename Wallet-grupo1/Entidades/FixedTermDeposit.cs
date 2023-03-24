f823714a422ff594a0ca8c3a8a20da8b09bec771
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-03-24 16:14:35 -0300

    mod entidad

--- Wallet-grupo1/Entidades/FixedTermDeposit.cs
+++ Wallet-grupo1/Entidades/FixedTermDeposit.cs
@@ -1,18 +1,29 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Wallet_grupo1.Entidades
 {
-    public class FixedTermDeposits
+    [Table("FixedTermDeposits")]
+    public class FixedTermDeposit
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
 
-        public int Id { get; set; };
-        public string product_description { get; set; } = null!;
-        public string image { get; set; } = null!;
-        public double points { get; set; } 
+        [Required]
+        [Column("product_description")]
+        [StringLength(250)]
+        public string ProductDescription { get; set; } = null!;
 
+        [Required]
+        [Column("image")]
+        [StringLength(250)]
+        public string Image { get; set; } = null!;
+
+        [Required]
+        [Column("points")]
+        public double Points { get; set; }
     }
-
-
 }
-
