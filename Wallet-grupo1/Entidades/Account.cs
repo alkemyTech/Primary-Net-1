f2559eef7c0924fd0aa048e1fa554c84185c49ee
juanpaladino15 <juanpaladino15@gmail.com>
2023-03-24 10:43:49 -0300

    add: detalles de migracion

--- Wallet-grupo1/Entidades/Account.cs
+++ Wallet-grupo1/Entidades/Account.cs
@@ -1,17 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Wallet_grupo1.Entidades
 {
+    [Table("Accounts")]
     public class Account
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public DateOnly creationDate { get; set; }
-        public double money { get; set; }
+
+        [Required]
+        public DateTime creationDate { get; set; }
+
+        [Required]
+        [Precision(18,2)]
+        public decimal money { get; set; }
+
+        [Required]
         public bool isBlocked { get; set; }
+
+        [Required]
         public int user_id { get; set; }
 
         [ForeignKey("user_id")]
         public virtual int User_id{ get; set; }
-
     }
 }
