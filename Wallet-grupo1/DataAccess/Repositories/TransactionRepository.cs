f27b32ddf2995ec42d69a8eaa03a930c5599966b
juanpaladino15 <82821453+juanpaladino15@users.noreply.github.com>
2023-03-26 10:14:04 -0300

    add: TransactionController y  Transactionrepository

--- Wallet-grupo1/DataAccess/Repositories/TransactionRepository.cs
+++ Wallet-grupo1/DataAccess/Repositories/TransactionRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 using Wallet_grupo1.Entidades;
 
@@ -12,31 +13,102 @@ namespace Wallet_grupo1.DataAccess.Repositories
             _context = context;
         }
 
-        public override async List<Transaction> GetAll()
+        public override async Task<List<Transaction>> GetAll()
         {
             try
             {
-                return await 
+                return await _context.Set<Transaction>().ToListAsync();
             }
-            catch { Exception ex}
+            catch (Exception ex)
+            {
+                
+            }
+
+        }
+
+        public async Task<Transaction> GetByid(int id)
+        {
+
+        }
+
+        public async void Insert(Transaction transaction)
+        {
+            try 
+            {
+                var existingTransaction = await _context.Transactions.Where(x => x.Id == transaction.Id).FirstOrDefaultAsync();
+
+                if (existingTransaction == null)
+                {
+                    _context.Set<Transaction>().Add(transaction);
+                }
+            }
+            catch (Exception ex)
             {
 
             }
         }
-        public void Insert(Transaction transaction)
+        //public void Insert(Transaction transaction)
+        //{
+        //    _context.Transactions.Insert(transaction);
+        //}
+
+        public async void Delete(Transaction transaction)
         {
-            _context.Transactions.Insert(transaction);
+            try
+            {
+                var existingTransaction = await _context.Transactions.Where(x => x.Id == transaction.Id).FirstOrDefaultAsync();
+
+                if (existingTransaction != null)
+                {
+                   _context.Set<Transaction>().Remove(transaction);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        
+        
+        //public void Delete(Transaction transaction)
+        //{
+        //    _context.Transactions.Delete(transaction);
+        //}
+
+
+        public override async void Update(Transaction transaction)
+        {
+            
+            try
+            {
+                var existingTransaction = await _context.Transactions.Where(x => x.Id == transaction.Id).FirstOrDefaultAsync();
+
+                if (existingTransaction == null)
+                {
+                    _context.Set<Transaction>().Add(transaction);
+                }
+                else
+                {
+                    existingTransaction.Amount = transaction.Amount;
+                    existingTransaction.Date = transaction.Date;
+                    existingTransaction.Type = transaction.Type;
+
+                    //Las claves foraneas se actualizan??
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+
         }
 
-        public void Delete(Transaction transaction)
-        {
-            _context.Transactions.Delete(transaction);
-        }
-
-        public void Update(Transaction transaction)
-        { 
-            _context.Transactions.Update(transaction);
-        }
+        //public void Update(Transaction transaction)
+        //{ 
+        //    _context.Transactions.Update(transaction);
+        //}
         public void SaveChanges()
         {
             _context.SaveChanges();
