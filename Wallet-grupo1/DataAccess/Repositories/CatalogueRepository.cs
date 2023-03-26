70d38776bf14a699ae771d9efc81942f3f3c7364
juanpaladino15 <82821453+juanpaladino15@users.noreply.github.com>
2023-03-26 10:20:57 -0300

    arreglos de pull

---
+++ Wallet-grupo1/DataAccess/Repositories/CatalogueRepository.cs
@@ -0,0 +1,11 @@
+﻿using Wallet_grupo1.Entidades;
+
+namespace Wallet_grupo1.DataAccess.Repositories;
+
+public class CatalogueRepository : Repository<Catalogue>
+{
+    public CatalogueRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+}
