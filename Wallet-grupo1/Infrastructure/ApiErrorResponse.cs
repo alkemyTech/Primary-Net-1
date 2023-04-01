1caaaaa96ca73b21ac853beab93785341602222f
Benjamin <benjagarate@hotmail.com>
2023-04-01 17:48:25 -0300

    PNT1-110: Agregado ResponseFactory para formato de respuestas
    Se agrego una clase encargada de generar las respuestas con el formato solicitado. Tiene dos metodos, uno encargado de crear un respuesta exitosa con codigo HTTP + la respuesta en si y otro para una respuesta de error con codigo HTTP + la lista de errores.

---
+++ Wallet-grupo1/Infrastructure/ApiErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Wallet_grupo1.Infrastructure;
+
+public class ApiErrorResponse
+{
+    public int Status { get; set; }
+    public List<ResponseError>? Errors { get; set; }
+
+    public class ResponseError
+    {
+        public string? Error { get; set; }
+    }
+}   
---
+++ Wallet-grupo1/Infrastructure/ApiSuccessfullResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Wallet_grupo1.Infrastructure;
+
+public class ApiSuccessfullResponse
+{
+    public int Status { get; set; }
+    public object? Data { get; set; }
+}
