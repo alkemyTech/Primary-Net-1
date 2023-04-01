d6fb991897caa0de4125f9b871f9ed72339ed3ef
Benjamin <benjagarate@hotmail.com>
2023-04-01 19:50:27 -0300

    PNT1-92: Logica de paginacion e implementacion en catalogue
    Se agrego la logica generica para paginar una lista de elementos y su respectivo DTO para devolver la respuesta en el controlador. Ademas lo implemente en el controlador del Catalogue.

---
+++ Wallet-grupo1/DTOs/PaginateDataDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Wallet_grupo1.DTOs;
+
+public class PaginateDataDto<T>
+{
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalItems { get; set; }
+    public string PrevUrl { get; set; }
+    public string NextUrl { get; set; }
+    public List<T> Items { get; set; } 
+}
