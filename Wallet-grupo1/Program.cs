25ae59fd2d5a745c42282fcf7faefec1802679bb
Benjamin <benjagarate@hotmail.com>
2023-04-02 17:07:44 -0300

    PNT1-99: Agregar Documentación a Roles
    Se agregó la documentación en cada uno de los endpoints del controlador de Roles. Además de eso se configuro el proyecto y Swagger para que muestre los comentarios, de esta forma se puede visualizar en la UI de Swagger dicha documentación.

--- Wallet-grupo1/Program.cs
+++ Wallet-grupo1/Program.cs
@@ -1,9 +1,11 @@
+using System.Reflection;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Wallet_grupo1.Controllers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Wallet_grupo1;
 using Wallet_grupo1.DataAccess;
 using Wallet_grupo1.Services;
@@ -15,7 +17,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Wallet Grupo 1", Version = "v1" });
+
+    // Añadir comentarios XML a Swagger
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWorkService>();
 
@@ -48,7 +58,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Wallet Grupo 1"));
 }
 
 app.UseHttpsRedirection();
