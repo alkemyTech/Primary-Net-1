20f0f36a89f58a0d2a759055ae4e52414ea0a583
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-04-02 16:11:17 -0300

    PNT1-74:Editar usuario

--- Wallet-grupo1/Controllers/UserController.cs
+++ Wallet-grupo1/Controllers/UserController.cs
@@ -7,6 +7,8 @@ using Wallet_grupo1.Services;
 using Wallet_grupo1.Logic;
 using Wallet_grupo1.Entities;
 using Wallet_grupo1.Helpers;
+using Wallet_grupo1.DTOs;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Wallet_grupo1.Controllers
 {
@@ -34,7 +36,7 @@ namespace Wallet_grupo1.Controllers
         {
             var user = await _unitOfWorkService.UserRepo.GetById(id);
             if (user is null) return NotFound();
-        
+
             return Ok(user);
         }
 
@@ -45,29 +47,57 @@ namespace Wallet_grupo1.Controllers
             var user = await _unitOfWorkService.UserRepo.GetById(id);
 
             if (user is null) return NotFound($"No se encontro ningun user con el id: {id}.");
-                
+
             var result = await _unitOfWorkService.UserRepo.Delete(user);
 
             if (!result)
                 return StatusCode(500, $"No se pudo eliminar el user con id: {id}" +
                                        $" porque no existe o porque no se pudo completar la transacción.");
-                                       
+
             await _unitOfWorkService.Complete();
 
             return Ok();
         }
-
+        [Authorize]
         [HttpPut("{id}")]
-        public async Task<ActionResult> Update(int id, User user)
+        public async Task<ActionResult> Update([FromRoute] int id, [FromBody] UserDto user)
         {
-            var result = await _unitOfWorkService.UserRepo.Update(user);
-            
+            string? authorizationHeader = Request.Headers["Authorization"];
+
+            if (authorizationHeader is null) return Unauthorized("No se proporcionó un token de seguridad.");
+
+            if (string.IsNullOrEmpty(authorizationHeader) || !authorizationHeader.StartsWith("Bearer "))
+                return Unauthorized("No se proporcionó un token de seguridad válido.");
+
+            string jwtToken = authorizationHeader.Substring(7);
+
+            // Extraigo el userid del token (es un claim)
+            var userIdToken = TokenJwtHelper.ObtenerUserIdDeToken(jwtToken);
+            if (userIdToken is null) throw new SecurityTokenException("El token no tiene el claim del user id.");
+
+
+
+            var userToken = _unitOfWorkService.AccountRepo.GetById(id).Result;
+
+            if (userToken is null) return NotFound($"No se encontró ningun user con el id: {id}.");
+
+            // Valido que sea el mismo user el loggeado y el dueño de la cuenta.
+            if (userToken.UserId != id)
+                return Forbid("La cuenta no pertenece al usuario loggeado.");
+
+
+
+
+
+            var result = await _unitOfWorkService.UserRepo.Update(new Entities.User(user, id));
+
+
             if (!result)
-                return StatusCode(500, $"No se pudo actualizar el user con id: {user.Id}" +
-                                       $" porque no existe o porque no se pudo completar la transacción."); 
-                                       
+                return StatusCode(500, $"No se pudo actualizar el user con id: {id}" +
+                                       $" porque no existe o porque no se pudo completar la transacción.");
+
             await _unitOfWorkService.Complete();
-            
+
             return Ok();
         }
     }
