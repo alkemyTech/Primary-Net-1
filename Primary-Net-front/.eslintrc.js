6423aa38ccb9cb60774ab16146a5861797142183
Matias Enrique <mati.seba.enrique@gmail.com>
2023-04-11 09:45:53 -0300

    reestructuracion del proyecto para incluir front

---
+++ Primary-Net-front/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    semi: [1, 'always'],
+    'react/react-in-jsx-scope': 'off',
+    'space-before-function-paren': 'off',
+    'react/prop-types': 'off'
+  }
+};
