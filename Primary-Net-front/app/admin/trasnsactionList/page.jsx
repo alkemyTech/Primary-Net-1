f11ef788d7d1ed0d7708c1b72444cc42da96519c
juanpaladino15 <juanpaladino15@gmail.com>
2023-04-13 16:05:43 -0300

    PNT1-117: Ahora funciona

--- Primary-Net-front/app/admin/trasnsactionList/page.jsx
+++ Primary-Net-front/app/admin/trasnsactionList/page.jsx
@@ -1,31 +1,38 @@
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { getServerSession } from "next-auth";
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-import React from "react";
+import React from 'react';
 
-export default async function TransactionList(){
-    const session = await getServerSession(authOptions);
-    if(session){
-        const data = await fetch('https://localhost:7131/api/transaction',{
-            headers:{
-                'Content-Type': 'application/json',
-                Authorization: 'Baearer ' + session.user.accessToken
-            },
-            cache: 'no-store'
-        }).catch((err)=>{
-            throw new Error(err.message);
-        });
+export default async function FixedTermList() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    const data = await fetch('https://localhost:7131/api/transaction', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + session.user.accessToken
+      },
+      cache: 'no-store'
+    }).catch((err) => {
+      throw new Error(err.message);
+    });
 
-        const transactions = await data.json();
+    const transactions = await data.json();
 
-        return(
-            <div>
-                <ul>
-                    {transactions.data.items.map((item)=>(
-                        <li key={item.id}>{item.concept}</li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+    console.log(transactions);
+
+    return (
+      <div>
+        <h1>Lista de elementos:</h1>
+        <ul>
+          {transactions.data.items.map((item) => (
+            <li key={item.id}>
+              Amount:{item.amount} concepto:{item.concept} tipo:{item.type}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  return redirect('/');
 }
