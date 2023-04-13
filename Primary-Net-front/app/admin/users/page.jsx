49f7a46962e903306576e4e7f4c30520cbb079f1
Matias Enrique <mati.seba.enrique@gmail.com>
2023-04-13 10:08:55 -0300

    commit

--- Primary-Net-front/app/admin/users/page.jsx
+++ Primary-Net-front/app/admin/users/page.jsx
@@ -1,31 +1,7 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
-import { getServerSession } from 'next-auth';
-import axios from 'axios';
-
 import React from 'react';
 
-export default async function UsersList() {
-  const session = await getServerSession(authOptions);
-  const users = await axios
-    .get('https://localhost:7131/api/user', {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + session.accessToken
-      }
-    })
-    .catch((err) => {
-      throw new Error(err.message);
-    });
-  if (session) {
-    return (
-      <div>
-        <h1>Lista de elementos:</h1>
-        <ul>
-          {users.data.map((user) => (
-            <li key={user.id}>{user.firstName}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function UserDetail() {
+  return <div>UserDetail</div>;
 }
+
+export default UserDetail;
