ebf4357e48f26b6698b0a1495e131282db4c3662
Martinrubio03 <99345280+Martinrubio03@users.noreply.github.com>
2023-04-13 19:23:43 -0300

    Agrego listado Catalogue y Account

---
+++ Primary-Net-front/app/ListCatalogue/page.jsx
@@ -0,0 +1,38 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+import React from 'react';
+
+export default async function UsersList() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    const data = await fetch('https://localhost:7131/api/Catalogue', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + session.user.accessToken
+      },
+      cache: 'no-store'
+    }).catch((err) => {
+      throw new Error(err.message);
+    });
+
+    const accounts = await data.json();
+
+    console.log(accounts);
+
+    return (
+      <div>
+        <h1>Lista de elementos:</h1>
+        <ul>
+          {accounts.data.items.map((item) => (
+            <li key={item.id}>
+              {item.id} {item.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  return redirect('/');
+}
